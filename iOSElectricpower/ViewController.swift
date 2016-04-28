6f8cb6f753b21f8fbf33c5407209626b0faca873
Chotipat Top <topz.classic@gmail.com>
2016-04-28 12:41:58 +0700

    Initial Commit

---
+++ iOSElectricpower/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSElectricpower
+//
+//  Created by Chotipat on 4/28/2559 BE.
+//  Copyright © 2559 Chotipat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
